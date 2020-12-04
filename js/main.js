bb9a42e761d6a66d4c2904ac30e49166a21b4b29
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-12-04 17:32:08 -0600

    functions to generate rows ok

--- js/main.js
+++ js/main.js
@@ -39,6 +39,36 @@ const cleanForm = () => {
   calories.value = ''
 }
 
+//function composition
+const tag = (t) => {
+  if(typeof t === 'string'){
+    tagAttrs({tag: t})
+  }else{
+    tagAttrs(t)
+  }
+}
+
+const tableRowTag = tag('tr')
+const tableRow = items => compose(tableRowTag,tableCells)(items)
+
+const tableCell = tag('td')
+const tableCells = items => items.map(tableCell).join('')
+
+const attrsToString = (obj = {}) => {
+  const keys = Object.keys(obj)
+  const attrs = []
+
+  for(let i=0;i<keys.length;i++){
+    let attr = keys[i]
+    attrs.push(`${attr}="${obj[attr]}"`)
+  }
+  const string = attrs.join('')
+  return string
+}
+
+const tagAttrs = obj => (content = "") =>
+  `<${obj.tag}${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
+
 description.addEventListener('keypress',()=>{
   description.classList.remove('is-invalid')
 })
@@ -54,3 +84,4 @@ protein.addEventListener('keypress',()=>{
 calories.addEventListener('keypress',()=>{
   calories.classList.remove('is-invalid')
 })
+
