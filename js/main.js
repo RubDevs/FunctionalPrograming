75501d9e435e6696b13bb39971aefbe6de5829a3
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-12-04 16:04:02 -0600

    fields validation

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+let description = document.getElementById('description')
+let carbs = document.getElementById('carbs')
+let protein = document.getElementById('protein')
+let calories = document.getElementById('calories')
+
+const validateInputs = () => {
+  description.value ? '' : description.classList.add('is-invalid')
+  carbs.value ? '' : carbs.classList.add('is-invalid')
+  protein.value ? '' : protein.classList.add('is-invalid')
+  calories.value ? '' : calories.classList.add('is-invalid')
+
+  if(description.value && calories.value && carbs.value && protein.value){
+    console.log('Ok')
+  }
+}
+
+description.addEventListener('keypress',()=>{
+  description.classList.remove('is-invalid')
+})
+
+carbs.addEventListener('keypress',()=>{
+  carbs.classList.remove('is-invalid')
+})
+
+protein.addEventListener('keypress',()=>{
+  protein.classList.remove('is-invalid')
+})
+
+calories.addEventListener('keypress',()=>{
+  calories.classList.remove('is-invalid')
+})
