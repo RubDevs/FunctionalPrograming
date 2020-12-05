be4200681146d6b3ac44059a5150fc8ad54fd3c8
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-12-04 18:45:48 -0600

    show and delete items

--- js/main.js
+++ js/main.js
@@ -1,89 +1,131 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
-let description = document.getElementById('description')
-let carbs = document.getElementById('carbs')
-let protein = document.getElementById('protein')
-let calories = document.getElementById('calories')
-let totalCarbs = document.getElementById('totalCarbs')
-let totalProtein = document.getElementById('totalProtein')
-let totalCalories = document.getElementById('totalCalories')
-
-let list = []
-
-const validateInputs = () => {
-  description.value ? '' : description.classList.add('is-invalid')
-  carbs.value ? '' : carbs.classList.add('is-invalid')
-  protein.value ? '' : protein.classList.add('is-invalid')
-  calories.value ? '' : calories.classList.add('is-invalid')
-
-  if(description.value && calories.value && carbs.value && protein.value){
-    addToList()
-    console.log(list)
-    cleanForm()
-    updateTotals()
-  }
-}
-
-const addToList = () => {
-  const newElement = {
-    description: description.value,
-    carbs: parseInt(carbs.value),
-    protein: parseInt(protein.value),
-    calories: parseInt(calories.value)
-  }
-
-  list.push(newElement)
-}
-
-const updateTotals = () => {
-  let carbs = 0, protein = 0, calories = 0
-  list.map((item) => {
-    carbs += item.carbs,
-    protein += item.protein,
-    calories += item.calories
-  })
-  totalCarbs.textContent = carbs
-  totalProtein.textContent = protein
-  totalCalories.textContent = calories
-}
-
-const cleanForm = () => {
-  description.value = ''
-  carbs.value = ''
-  protein.value = ''
-  calories.value = ''
-}
-
-//function composition
-const tag = (t) => {
-  if(typeof t === 'string'){
-    tagAttrs({tag: t})
-  }else{
-    tagAttrs(t)
-  }
-}
-
-const tableRowTag = tag('tr')
-const tableRow = items => compose(tableRowTag,tableCells)(items)
-
-const tableCell = tag('td')
-const tableCells = items => items.map(tableCell).join('')
-
 const attrsToString = (obj = {}) => {
   const keys = Object.keys(obj)
   const attrs = []
 
-  for(let i=0;i<keys.length;i++){
+  for (let i = 0; i < keys.length; i++) {
     let attr = keys[i]
     attrs.push(`${attr}="${obj[attr]}"`)
   }
-  const string = attrs.join('')
+
+  const string = attrs.join(' ')
+
   return string
 }
 
-const tagAttrs = obj => (content = "") =>
-  `<${obj.tag}${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
+const tagAttrs = obj => (content = '') =>
+  `<${obj.tag}${obj.attrs ? ' ' :	 ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
+
+const tag = t => {
+  if(typeof t === 'string') {
+    return tagAttrs({ tag: t })
+  }
+  return tagAttrs(t)
+}
+
+const tableRowTag = tag('tr')
+const tableRow = items => compose(tableRowTag, tableCells)(items)
+
+const tableCell = tag('td')
+const tableCells = items => items.map(tableCell).join('')
+
+const trashIcon = tag({tag: 'i', attrs: {class: 'fas fa-trash-alt'}})('')
+
+let description = $('#description')
+let calories = $('#calories')
+let carbs = $('#carbs')
+let protein = $('#protein')
+
+let list = []
+
+description.keypress(() => {
+  description.removeClass('is-invalid')
+})
+
+calories.keypress(() => {
+  calories.removeClass('is-invalid')
+})
+
+carbs.keypress(() => {
+  carbs.removeClass('is-invalid')
+})
+
+protein.keypress(() => {
+  protein.removeClass('is-invalid')
+})
+
+const validateInputs = () => {
+
+  description.val() ? '' : description.addClass('is-invalid')
+  calories.val() ? '' : calories.addClass('is-invalid')
+  carbs.val() ? '' : carbs.addClass('is-invalid')
+  protein.val() ? '' : protein.addClass('is-invalid')
+
+  if(
+    description.val() &&
+    calories.val() &&
+    carbs.val() &&
+    protein.val()
+  ) add()
+}
+
+const add = () => {
+  const newItem = {
+    description: description.val(),
+    calories: parseInt(calories.val()),
+    carbs: parseInt(carbs.val()),
+    protein: parseInt(protein.val())
+  }
+
+  list.push(newItem)
+  updateTotals()
+  cleanInputs()
+  renderItems()
+}
+
+const updateTotals = () => {
+  let calories = 0, carbs = 0, protein = 0
+
+  list.map(item => {
+    calories += item.calories,
+    carbs += item.carbs,
+    protein += item.protein
+  })
+
+  $('#totalCalories').text(calories)
+  $('#totalCarbs').text(carbs)
+  $('#totalProtein').text(protein)
+}
+
+const cleanInputs = () => {
+  description.val('')
+  calories.val('')
+  carbs.val('')
+  protein.val('')
+}
+
+const renderItems = () => {
+  $('tbody').empty()
+
+  list.map((item, index) => {
+    const removeButton = tag({
+      tag: 'button',
+      attrs: {
+        class: 'btn btn-outline-danger',
+        onclick: `removeItem(${index})`
+      }
+    })(trashIcon)
+    $('tbody').append(tableRow([item.description, item.calories, item.carbs, item.protein, removeButton]))
+  })
+}
+
+const removeItem = (index) => {
+  list.splice(index,1)
+  updateTotals()
+  renderItems()
+}
 
 description.addEventListener('keypress',()=>{
   description.classList.remove('is-invalid')
